dc6b7dbf7eab6d05daf081a36e1c23c280f67ead
Faha91 <kamardinovfakhriddin@gmail.com>
2019-08-13 18:40:06 -0700

    Modified Test class

--- src/test/java/briteERP/tests/component/ContractTest.java
+++ src/test/java/briteERP/tests/component/ContractTest.java
@@ -1,10 +1,13 @@
 package briteERP.tests.component;
 
+import com.github.javafaker.Faker;
 import org.testng.annotations.Test;
 
 public class ContractTest {
+    static public Faker faker = new Faker();
     @Test
     public void Test1(){
+        System.out.println("driver open vytrcak" +faker.chuckNorris().fact());
 
     }
 }
